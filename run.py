886a818f8c3f457847126919d55eab0faa3dbeca
JohannHelbert <>
2023-06-14 10:53:19 +0000

    Add variables and a print statement

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+suits = ["spades", "clubs", "hearts", "diamonds"]
+suit = suits[2]
+rank = "K"
+value = 10
+print("Your card is: " + rank + " of " + suit)
